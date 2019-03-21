92892efa89377ac87139fb1ff73e1c14d237cf52
Tatsiana Charnavokaya <4ernookaya.tanya@gmail.com>
2019-03-21 11:59:15 +0300

    feat: add new solution

--- src/index.js
+++ src/index.js
@@ -2,45 +2,61 @@
 module.exports = function check(str, bracketsConfig) {
 
 	let arrayChars = str.split('');
-	let stack = [];
-	let lastElemStack = 0;
+	
 
 	for (let i = 0; i < arrayChars.length; i++) {
+		let firstSimbol = arrayChars[i];
+		let secondSimbol = arrayChars[i+1];
 
 		for (let j = 0; j < bracketsConfig.length; j++) {
-
 			let opening = bracketsConfig[j][0];
 			let closening = bracketsConfig[j][1];
 
-			if (opening == closening) {
-				lastElemStack = stack[stack.length - 1];
-				if (lastElemStack == opening) {
-					stack.pop();
-          break;
-          
-				} else {
-					stack.push(arrayChars[i]);
-					break;
-				}
-			} else {
-				if (arrayChars[i] == opening) {
-					stack.push(arrayChars[i]);
-					break;
-
-				} else if (arrayChars[i] == closening) {
-					lastElemStack = stack[stack.length - 1];
-            if (lastElemStack == opening) {
-              stack.pop();
-              break;
-            } 
-				}
+			if (firstSimbol == opening && secondSimbol == closening) {
+				arrayChars.replace(i,'');
+				let result = check(arrayChars.join(''), bracketsConfig);
+				return result;
 			}
 		}
 	}
-
-	if (stack.length == 0) {
+	if (arrayChars.length == 0) {
 		return true;
 	} else {
 		return false;
 	}
 }
+
+		// don't pass 12 and 15 tests!!
+
+		// let stack = [];
+		// let lastElemStack = 0;
+
+		// 	if (opening == closening) {
+		// 		lastElemStack = stack[stack.length - 1];
+		// 		if (lastElemStack == opening) {
+		// 			stack.pop();
+        //   break;
+          
+		// 		} else {
+		// 			stack.push(arrayChars[i]);
+		// 			break;
+		// 		}
+		// 	} else {
+		// 		if (arrayChars[i] == opening) {
+		// 			stack.push(arrayChars[i]);
+		// 			break;
+
+		// 		} else if (arrayChars[i] == closening) {
+		// 			lastElemStack = stack[stack.length - 1];
+        //     if (lastElemStack == opening) {
+        //       stack.pop();
+        //       break;
+        //     } 
+		// 		}
+			// }
+
+	// if (stack.length == 0) {
+	// 	return true;
+	// } else {
+	// 	return false;
+	// }
